09e3e9f154102d1905f6eb7aa33828dda0c68ecc
Marcos <marcos.k.santos6@aluno.senai.br>
2026-06-26 15:56:50 -0300

    CALCULO FINAL CRIADO

--- js/calculo.js
+++ js/calculo.js
@@ -2,15 +2,15 @@ const calcResiduo = (objEmpresa) => {
     let resultCalc =''
 
     if ((objEmpresa.residuo <= 4)){
-        resultCalc = 'NÃO ATINGIU A META MÍNIMA'
+        resultCalc = 0
     }else if((objEmpresa.residuo <= 46)){
-         resultCalc = `PARABENS! VOCÊ RECEBEU :${((objEmpresa.residuo * 220)* 0.08).toFixed(2).replace('.',',')}R$ DE BONÛS`
+         resultCalc = (parseFloat(objEmpresa.residuo * 220)* 0.08)
     }else if((objEmpresa.residuo <= 68)){
-          resultCalc = `PARABENS! VOCÊ RECEBEU :${((objEmpresa.residuo * 220)* 0.10).toFixed(2).replace('.',',')}R$ DE BONÛS`
+          resultCalc = (parseFloat(objEmpresa.residuo * 220)* 0.10)
     }else if((objEmpresa.residuo <= 113)){
-         resultCalc = `PARABENS! VOCÊ RECEBEU :${((objEmpresa.residuo * 220)* 0.15).toFixed(2).replace('.',',')}R$ DE BONÛS`
+         resultCalc = (parseFloat(objEmpresa.residuo * 220)* 0.15)
     }else {
-         resultCalc = `PARABENS! VOCÊ RECEBEU :${((objEmpresa.residuo * 220)* 0.20).toFixed(2).replace('.',',')}R$ DE BONÛS`
+         resultCalc = (parseFloat(objEmpresa.residuo * 220)* 0.20)
     }
      return resultCalc
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import {calcResiduo} from "./calculo.js"
 
+
 const formColeta = document.querySelector('#form-coleta')
 const divResult = document.querySelector('#div-result')
 
@@ -37,12 +38,11 @@ const listEmpresas = () => {
     empresas.forEach((elem, i) => {
 
         const valorResiduo = elem.residuo * 220;
-         
+        const valorF = valorResiduo + calcResiduo(elem);
 
         divResult.innerHTML += `${i + 1} ${elem.nome} <br>
          ${elem.descricao} <br>
          ${parseFloat(elem.residuo).toFixed(2).replace('.',',')}t - ${calcResiduo(elem)} <br>
-        ${parseFloat(valorResiduo).toFixed(2).replace('.',',')} <br>
-        ${parseFloat(somaFinal).toFixed(2).replace('.',',')}`
+        ${parseFloat(valorResiduo).toFixed(2).replace('.',',')} <br>`
     })
 }
